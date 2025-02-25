7392f33eff46530f9004d5c2fdef2e0e56304bab
K <mustkre8@gmail.com>
2025-02-25 08:33:21 -0800

    notes

---
+++ src/routes/api/auth/+server.svelte
@@ -0,0 +1,13 @@
+<!-- 
+ In this module we want to provide API for the svelte app to authenticate. enabling:
+ - login
+ - register
+ - verify
+ - refresh
+ - logout
+
+The spec for auth API is available in `src/lib/routes/api/auth-spec.yaml`
+
+Note: the base url for AuthAPI is https://ikhsfhbxnd.execute-api.us-east-1.amazonaws.com/Prod
+
+-->
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,9 @@
+<!--
+in this path/route page(s) we want to exercise the api/auth route to perform:
+- login
+- register
+- logout
+- verify
+
+We want to store token returned from auth service to use in API calls later. 
+-->
